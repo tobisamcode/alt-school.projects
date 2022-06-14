997ea6c1537625c8afce971d9126cbbc301e17b1
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-06-14 13:02:18 +0100

    functions

--- JavaScript/week-one/project.js
+++ JavaScript/week-one/project.js
@@ -1,19 +1,23 @@
-let username = prompt("Enter your username");
-while (ValidateUsername(username) == false) {
-  username = prompt("Username not valid, Please enter a valid username");
+function displayUserDetails() {
+  let username = prompt("Enter your username");
+  while (ValidateUsername(username) == false) {
+    username = prompt("Username not valid, Please enter a valid username");
+  }
+
+  let password = prompt("Enter your password");
+  while (ValidatePassword(password) == false) {
+    password = prompt("Password is not valid, Plaese enter a valid password");
+  }
+
+  console.log(username, password);
 }
 
-let password = prompt("Enter your password");
-while (ValidatePassword(password) == false) {
-  password = prompt("Password is not valid, Plaese enter a valid password");
-}
-
-console.log(username, password);
+displayUserDetails();
 alert("you have come to the end of the programm");
 
 function ValidateUsername(username) {
   if (username == null) {
-    return false;
+    return true;
   }
 
   if (username.length > 10) {
@@ -25,7 +29,7 @@ function ValidateUsername(username) {
 
 function ValidatePassword(password) {
   if (password == null) {
-    return false;
+    return true;
   }
 
   if (password.length < 6) {
