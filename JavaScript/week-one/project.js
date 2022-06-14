fb87504cf5e7956cf6226689dcf55ea68361cf84
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-06-14 12:07:08 +0100

    functions

--- JavaScript/week-one/project.js
+++ JavaScript/week-one/project.js
@@ -1,1 +1,9 @@
-const name = "Tobi";
+let username = prompt("Enter your username");
+
+function ValidateUsername(username) {
+  if (username.length > 10) {
+    return false;
+  } else {
+    return true;
+  }
+}
