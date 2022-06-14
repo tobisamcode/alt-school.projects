d00852d5e2c720d8fa387c91363cbe64b9c0c5b9
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-06-14 17:47:44 +0100

    functions

---
+++ JavaScript/week-three/array.js
@@ -0,0 +1,38 @@
+// Crreate an empty array
+
+// let user = ["John", "Mary", 20.4, 20, { theme: "dark" }];
+// console.log(user);
+// console.log(user.length);
+
+// create an array
+const animals = ["Tiger", "Lion", "pig"];
+
+// Get values from the array
+// alert(animals[1]);
+// alert(animals[2]);
+
+// Update a value in the array
+// animals[2] = "Goat";
+// console.log(animals);
+
+// to add new element to an array
+// animals.push("cow");
+// alert(animals);
+
+// Remove a avlue from the end of an array
+// animals.pop();
+
+// Remove a value from the beginning of an array
+// animals.shift();
+
+// Add a avle to the beginning of an array
+// animals.unshift("cheetah");
+
+// Get the index of a value
+// console.log(animals.indexOf("pig"));
+// console.log(animals.indexOf("cow"));
+
+// Get the slice of an array
+// const animals = ["Tiger", "Lion", "pig", "Goat", "Hen", "Fish", "Cheetah"];
+
+// console.log(animals.slice(0, 3)); // ['Tiger', 'Lion', 'pig']
