e6d41c4663b169df0eeea7367718ea16055b3ce2
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-06-14 12:30:33 +0100

    functions

--- JavaScript/week-one/project.js
+++ JavaScript/week-one/project.js
@@ -1,6 +1,12 @@
 let username = prompt("Enter your username");
 
+console.log(ValidateUsername(username));
+
 function ValidateUsername(username) {
+  if (username == null) {
+    return false;
+  }
+
   if (username.length > 10) {
     return false;
   } else {
