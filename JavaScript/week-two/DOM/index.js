aa176918f2c99d443a32d92d3fea8a83bcc49f5d
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-06-15 14:24:29 +0100

    DOM AND BOM

--- JavaScript/week-two/DOM/index.js
+++ JavaScript/week-two/DOM/index.js
@@ -1,15 +1,15 @@
 // Display the title of the current page
 // alert(document.title);
 
+// setTimeout(() => {
+//   document.body.style.backgroundColor = "blue";
+// }, 3000);
+
 // Display the content of the head tag
-// alert(document.head.innerHTML);
+// console.log(document.head.innerHTML);
 
 // Display the content of the body tag
 // alert(document.body.innerHTML);
 
-
 //Task ==> Interact with DOM elements using the Developer Tools
 // E.g Wikipedia, Google, Twitter
-
-
-
