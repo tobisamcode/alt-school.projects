60aa6877d1af6ae334d227172d3123d3e195473f
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-06-15 14:10:45 +0100

    destructuring

---
+++ JavaScript/week-two/01/destructuring.js
@@ -0,0 +1,49 @@
+// // Destructuring an array
+// let names = ["Adebayo", "Tobi", "Akin"];
+
+// let [firstName, lastName, middleName] = names;
+// console.log(firstName, lastName, middleName);
+
+// Get the first name
+// let [name1] = names;
+// console.log(name1);
+
+// // Get the second name
+// let [, name2] = names;
+// console.log(name2);
+
+// // Get the third name
+// let [, , name3] = names;
+// console.log(name3);
+
+// Use the rest operator to get the rest of the values
+// let [name1, ...restOfNames] = names;
+// console.log(name1);
+// console.log(restOfNames); // ['Tobi', 'Akin']
+
+// Destructuring in Objects
+// let settings = { theme: "dark", fontSize: 24, fullScreen: true };
+
+// let { theme, fontSize, fullScreen } = settings;
+// console.log(theme, fontSize, fullScreen);
+
+// // Get the theme only and assign it to a newvariable
+// let { theme: newTheme } = settings;
+// console.log(newTheme);
+
+// Use the rest operator to get the rest of the settings
+// let { theme: newTheme1, ...restOfSettings } = settings;
+// console.log(newTheme1);
+// console.log(restOfSettings);
+
+// Destructuring in a Function
+// function getUserInfo({ name, age }) {
+//   console.log(`My name is ${name} and I am ${age} years old`);
+// }
+
+// const user = {
+//   name: "Tobi",
+//   age: 23
+// };
+
+// getUserInfo(user);
