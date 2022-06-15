fca1cb5e210d0b41c0bc0f2552bc0b317b024d27
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-06-15 11:28:03 +0100

    maps

--- JavaScript/week-two/destructuring.js
+++ JavaScript/week-two/destructuring.js
@@ -17,6 +17,21 @@ let [firstName, lastName, middleName] = names;
 // console.log(name3);
 
 // Use the rest operator to get the rest of the values
-let [name1, ...restOfNames] = names;
-console.log(name1);
-console.log(restOfNames); // ['Tobi', 'Akin']
+// let [name1, ...restOfNames] = names;
+// console.log(name1);
+// console.log(restOfNames); // ['Tobi', 'Akin']
+
+// Destructuring in Objects
+let settings = { theme: "dark", fontSize: 24, fullScreen: true };
+
+// let { theme, fontSize, fullScreen } = settings;
+// console.log(theme, fontSize, fullScreen);
+
+// // Get the theme only and assign it to a newvariable
+// let { theme: newTheme } = settings;
+// console.log(newTheme);
+
+// Use the rest operator to get the rest of the settings
+let { theme: newTheme1, ...restOfSettings } = settings;
+console.log(newTheme1);
+console.log(restOfSettings);
