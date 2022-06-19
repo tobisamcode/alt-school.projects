3174e23c35a1e2b063bc95eeff4907f2dfa818cd
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-06-19 11:57:34 +0100

    location.hash

--- JavaScript/week-two/BOM/index.js
+++ JavaScript/week-two/BOM/index.js
@@ -1,40 +1,43 @@
-// Location Object (https://developer.mozilla.org/en-US/docs/Web/API/Window/location)
+// // Location Object (https://developer.mozilla.org/en-US/docs/Web/API/Window/location)
 
-//Change location of web page
-alert(location.href); // shows current URL
+// location.hash
+var anchor = document.querySelector("a");
+console.log(anchor.hash);
 
-if (confirm("Open Google?")) {
-    location.href = "https://google.com"; // redirect the browser to another URL using the location object
-}
+// //Change location of web page
+// alert(location.href); // shows current URL
 
-// Reload the page
-if (confirm("Reload the page?")) {
-    location.reload();
-}
+// if (confirm("Open Google?")) {
+//     location.href = "https://google.com"; // redirect the browser to another URL using the location object
+// }
 
-// Change the URL
-if (confirm("Change the URL?")) {
-    location.href = "https://www.google.com";
-}
+// // Reload the page
+// if (confirm("Reload the page?")) {
+//     location.reload();
+// }
 
-// Change the URL and add a parameter
-if (confirm("Change the URL and add a parameter?")) {
-    location.href = "https://www.google.com?q=javascript";
-}
+// // Change the URL
+// if (confirm("Change the URL?")) {
+//     location.href = "https://www.google.com";
+// }
 
+// // Change the URL and add a parameter
+// if (confirm("Change the URL and add a parameter?")) {
+//     location.href = "https://www.google.com?q=javascript";
+// }
 
-// // Window Navigator (https://developer.mozilla.org/en-US/docs/Web/API/Window/navigator)
+// // // Window Navigator (https://developer.mozilla.org/en-US/docs/Web/API/Window/navigator)
 
-// // Get the browser name
-alert("You're browsing from: " + navigator.userAgent);
+// // // Get the browser name
+// alert("You're browsing from: " + navigator.userAgent);
 
-// // Get the browser version
-alert("You're using an engine created by : " + navigator.vendor);
+// // // Get the browser version
+// alert("You're using an engine created by : " + navigator.vendor);
 
-// Get the browser language
-alert("You're using a language: " + navigator.language);
+// // Get the browser language
+// alert("You're using a language: " + navigator.language);
 
-// Get user location
-navigator.geolocation.getCurrentPosition((position) => {
-    alert("You're at: " + position.coords.latitude + ", " + position.coords.longitude);
-});
+// // Get user location
+// navigator.geolocation.getCurrentPosition((position) => {
+//     alert("You're at: " + position.coords.latitude + ", " + position.coords.longitude);
+// });
