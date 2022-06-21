f49213e8c2e1ad7a5f3594a56ab0b4a380a6907e
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-06-21 23:46:03 +0100

    Callbacks Async Promises

--- JavaScript/week-three/async-await/better_callback.js
+++ JavaScript/week-three/async-await/better_callback.js
@@ -1,23 +1,9 @@
-// // A promise call cleanly handle multiple asynchronous operations.
-// function getData(url) {
-//     return new Promise((resolve, reject) => {
-//         if (!url) {
-//             reject("No URL provided");
-//         }
-
-//         const xhr = new XMLHttpRequest();
-//         xhr.open("GET", url);
-//         xhr.send();
-//         xhr.onload = function () {
-//             if (xhr.status === 200) {
-//                 resolve(xhr.responseText);
-//             } else {
-//                 reject(xhr.status);
-//             }
-
-//         };
-//     });
-// }
+// A promise call cleanly handle multiple asynchronous operations.
+async function getData(url) {
+  const response = await fetch(url);
+  const result = await response.json();
+  return result;
+}
 
 // async function getAllData() {
 //     const result = await getData(url);
@@ -28,17 +14,10 @@
 
 //     const result3 = await getData("https://reqres.in/api/users/2");
 //     console.log("Success 3", result3);
-    
+
 //     const result4 = await getData("https://reqres.in/api/unknown");
 //     console.log("Success 4", result4);
 // }
 
-
 // const url = prompt("Enter a URL");
 // getAllData(url)
-
-
-
-
-
-
