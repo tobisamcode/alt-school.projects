17ab980c9ff6289d4482dbf200c91ee29b4d5e70
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-06-21 22:13:10 +0100

    Callbacks Async Promises

--- JavaScript/week-three/async-await/index.js
+++ JavaScript/week-three/async-await/index.js
@@ -1,14 +1,17 @@
-// const url = prompt("Enter a URL");
+const url = prompt("Enter a URL");
 
 //Async before a function turns it into a promise
 //Await ensures a function is executed before continuing. It works if the function is asynchronous.
-// async function getData(url) {
-//     const response = await fetch(url);
-//     const result = await response.json();
-//     return result;
-// }
+async function getData(url) {
+  const response = await fetch(url);
+  const result = await response.json();
+  return result;
+}
 
-
+(async () => {
+  const result = await getData(url);
+  console.log("Success!", result);
+})();
 
 // getData(url)
 //     .then((result) => {
@@ -17,7 +20,6 @@
 //         console.log(`An error with status code ${status} occurred: `);
 //     });
 
-
 //Technically, the getData function is asynchronous, and we can do the following:
 // await getData(url) but we can't do that here because await is only valid within an async function.
 
@@ -25,12 +27,10 @@
 
 // A trick is to wrap the function in an IIFE async function.
 // (async () => {
-//     const result = await getData(url);
-//     console.log("Success!", result);
+//   const result = await getData(url);
+//   console.log("Success!", result);
 // })();
 
 //Handling errors
 // When using async/await, you can use try/catch to handle errors.
 // The catch block will run if an error occurs.
-
-
