47a40f38d1a0c1ff76915208bd37fb8f741237a3
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-06-26 02:43:57 +0100

    OOP

--- JavaScript/week-three/OOP/user_inherit.js
+++ JavaScript/week-three/OOP/user_inherit.js
@@ -1,92 +1,110 @@
 // Class inheritance is a way for one class to extend another class.
 
 class User {
+  constructor(firstName, lastName, email, password) {
+    this.firstName = firstName || "Anonymous";
+    this.lastName = lastName || "Superhuman";
+    this.email = email || "";
+    this.password = password || "defaultPassword";
+    this.emailActivated = false;
+  }
 
-    constructor(firstName, lastName, email, password) {
-        this.firstName = firstName || "Anonymous";
-        this.lastName = lastName || "Superhuman";
-        this.email = email || ""
-        this.password = password || "defaultPassword";
-        this.emailActivated = false;
-    }
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
 
-    getFullName() {
-        return `${this.firstName} ${this.lastName}`;
-    }
+  getEmail() {
+    return this.email;
+  }
 
-    getEmail() {
-        return this.email;
-    }
+  getPassword() {
+    return this.password;
+  }
 
-    getPassword() {
-        return this.password;
-    }
+  isEmailActivated() {
+    return this.emailActivated;
+  }
 
-    isEmailActivated() {
-        return this.emailActivated;
-    }
+  setFullName(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
 
-    setFullName(firstName, lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
+  setEmail(email) {
+    this.email = email;
+  }
 
-    setEmail(email) {
-        this.email = email;
-    }
+  setPassword(password) {
+    this.password = password;
+  }
 
-    setPassword(password) {
-        this.password = password;
-    }
-
-    activateEmail() {
-        this.emailActivated = true;
-    }
-
+  activateEmail() {
+    this.emailActivated = true;
+  }
 }
-
 
 class Student extends User {
-    constructor(firstName, lastName, email, password, studentNumber, studentClass, subjects) {
-        super(firstName, lastName, email, password); // Invoke the parent constructor with the same arguments
-        
-        // Add some more properties to customize the student class
-        this.studentNumber = studentNumber || "N/A";
-        this.studentClass = studentClass || "N/A";
-        this.subjects = subjects || []; 
-    }
+  constructor(
+    firstName,
+    lastName,
+    email,
+    password,
+    studentNumber,
+    studentClass,
+    subjects
+  ) {
+    super(firstName, lastName, email, password); // Invoke the parent constructor with the same arguments
 
-    getStudentNumber() {
-        return this.studentNumber;
-    }
+    // Add some more properties to customize the student class
+    this.studentNumber = studentNumber || "N/A";
+    this.studentClass = studentClass || "N/A";
+    this.subjects = subjects || [];
+  }
 
-    getStudentClass() {
-        return this.studentClass;
-    }
+  getStudentNumber() {
+    return this.studentNumber;
+  }
 
-    getSubjects() {
-        return this.subjects;
-    }
+  getStudentClass() {
+    return this.studentClass;
+  }
 
-    setStudentClass(studentClass) {
-        this.studentClass = studentClass;
-    }
+  getSubjects() {
+    return this.subjects;
+  }
 
-    setStudentNumber(studentNumber) {
-        this.studentNumber = studentNumber;
-    }
+  setStudentClass(studentClass) {
+    this.studentClass = studentClass;
+  }
 
-    addNewSubject(subject) {
-        this.subjects.push(subject);
-    }
+  setStudentNumber(studentNumber) {
+    this.studentNumber = studentNumber;
+  }
 
+  addNewSubject(subject) {
+    this.subjects.push(subject);
+  }
 }
 
-
 // Create some students
-const student1 = new Student("Obinna", "chikeobinna@mail.com", "mysimplepassword", 123, 1, ["English", "Mathematics"]);
-const student2 = new Student("Johnson", "Olamide", "jola@gmail.com", "somepassword12", 456, 2, ["English", "Science"]);
-
+const student1 = new Student(
+  "Obinna",
+  "Chike",
+  "chikeobinna@mail.com",
+  "mysimplepassword",
+  123,
+  1,
+  ["English", "Mathematics"]
+);
+const student2 = new Student(
+  "Johnson",
+  "Olamide",
+  "jola@gmail.com",
+  "somepassword12",
+  456,
+  2,
+  ["English", "Science"]
+);
 
 // We can still use methods from User class
 console.log(student1.getFullName());
